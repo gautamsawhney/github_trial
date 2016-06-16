4e5c83e73b137fbcfc387151d74b79121432513f
gautamsawhney <gsawhney21@gmail.com>
2016-06-16 09:06:52 -0400

    Generating Authtoken model

---
+++ spec/factories/authtokens.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :authtoken do
+    token "MyString"
+    last_used_at "2016-06-16 09:06:52"
+    sign_in_ip ""
+    user_agent "MyString"
+    device_id "MyString"
+    user nil
+  end
+end
